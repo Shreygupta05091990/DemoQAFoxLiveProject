f802da396a4b85b20df53123f18e29ff45bd9f88
ShreyGupta <Shreygupta33@gmail.com>
2021-03-06 18:28:36 +0530

    Feature B  branch to create merge conflict

--- src/test/java/com/tutorialsninja/automation/runner/Runner.java
+++ src/test/java/com/tutorialsninja/automation/runner/Runner.java
@@ -2,4 +2,4 @@ package com.tutorialsninja.automation.runner;
 //GIt fetch practice Attempt1
 public class Runner {
 // Now practising pushing into Git 
-}
+// Practising to push Feature B  from Shrey branch}
